38169b2bca531833bff78684ba5c1337e93d1a89
lbcdev <lbcdev@yahoo.com>
2017-07-12 23:02:49 -0400

    add encryption-based security

---
+++ src/org/sstore/security/SecurityManager.java
@@ -0,0 +1,12 @@
+package org.sstore.security;
+
+/**
+ * Security manager handles all operations related to data security in the
+ * system, including encryption, authentication, and etc.
+ * 
+ * @author lbchen
+ *
+ */
+public class SecurityManager {
+
+}
