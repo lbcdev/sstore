fbb38f650c61d3396c83cb7af1d951659fc7d265
lbcdev <lbcdev@yahoo.com>
2017-07-30 22:16:48 -0400

    cworkload

--- src/org/sstore/benchmarks/ConcurrentWorkload.java
+++ src/org/sstore/benchmarks/ConcurrentWorkload.java
@@ -1,5 +1,8 @@
 package org.sstore.benchmarks;
 
+import org.sstore.client.ClientRpcImpl;
+import org.sstore.utils.Constants;
+
 /**
  * Generate concurrent workload with multi-threading.
  * 
@@ -7,6 +10,14 @@ package org.sstore.benchmarks;
  *
  */
 public class ConcurrentWorkload {
+
+	ClientRpcImpl clientrpc;
+	String local, remote;
+	public ConcurrentWorkload() {
+		clientrpc = new ClientRpcImpl(Constants.METARPC_NAME);
+		local = "/Users/lbchen/got.png";
+		remote = "/Users/lbchen/data/exp/";
+	}
 
 	/**
 	 * Generate simple put workload with fix sized files.
@@ -25,5 +36,19 @@ public class ConcurrentWorkload {
 
 	public void simpleMix(int readPercent, int numOfClient, int putPerClient, int fileSize) {
 
+	}
+
+	class PutWorker implements Runnable {
+		int readnum;
+
+		public PutWorker(int readPerClient) {
+			readnum = readPerClient;
+		}
+
+		public void run() {
+			while (readnum-- > 0) {
+				clientrpc.putReq(local, remote + readnum);
+			}
+		}
 	}
 }
