22c36f3b84c91b9bd03b43a09066fa59dd64ed10
lbcdev <lbcdev@yahoo.com>
2017-08-04 23:52:03 -0400

    key mgmt server

---
+++ src/org/sstore/server/kms/KMServerRpc.java
@@ -0,0 +1,17 @@
+package org.sstore.server.kms;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * KMS rpc interface, defines all rpc calls.
+ * 
+ * @author lbchen
+ *
+ */
+public interface KMServerRpc extends Remote {
+	
+	public byte[] requestKey(long fileId) throws RemoteException;
+	
+	
+}
