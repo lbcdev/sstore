b3f2d7b45eb0d7f831a58646e5e43a73508ab2db
lbcdev <lbcdev@yahoo.com>
2017-08-04 23:39:53 -0400

    key mgmt server

---
+++ src/org/sstore/server/kms/KMServer.java
@@ -0,0 +1,12 @@
+package org.sstore.server.kms;
+
+/**
+ * Key management server stores, manages, and distributes keys for data.
+ * It can run on a separate server for better performance.
+ * 
+ * @author lbchen
+ *
+ */
+public class KMServer {
+
+}
--- src/org/sstore/server/test/TestKHTandKCache.java
+++ src/org/sstore/server/test/TestKHTandKCache.java
@@ -2,50 +2,83 @@ package org.sstore.server.test;
 
 import java.util.HashMap;
 
+import org.sstore.security.encryption.CipherHandler;
 import org.sstore.security.encryption.DataKeyGenerator;
+import org.sstore.utils.StreamFileUtils;
 
 public class TestKHTandKCache {
 
+	static CipherHandler handler;
 	private static HashMap<String, byte[]> kcache = new HashMap<String, byte[]>();
 	static long cid = 1838339;
 	static int klen = 16;
+	static String rootdir = "/Users/lbchen/";
+	static String in = rootdir + "got.png";
+	static String cout = rootdir + "cout.jpg";
+	static String dout = rootdir + "dout.jpg";
 
 	public static void main(String[] args) {
 		TestKHTandKCache test = new TestKHTandKCache();
-		int num = 100000;
+		int num = 5000;
 		long start, end;
 		test.init(num);
 		start = System.currentTimeMillis();
 		test.kht(num);
 		end = System.currentTimeMillis();
 		System.out.println(end - start);
-		
+		System.out.println("per op: " + (float)(end - start) / (float) num);
+		float kht_t = end - start;
+
 		start = System.currentTimeMillis();
 		test.kcache(num);
 		end = System.currentTimeMillis();
+		float kcache_t = end - start;
 		System.out.println(end - start);
-		System.out.println((kcache.size() * klen) + " byte");
+		System.out.println("per op: " + (float)(end - start) / (float) num);
+
+		System.out.println((kcache.size() * klen) / 1000 + " KB");
+		float percent = (kht_t - kcache_t) / kht_t;
+		System.out.println((int)(percent * 100) + "%");
 
 	}
 
 	void kht(int num) {
 		for (int i = 0; i < num; i++) {
+
 			DataKeyGenerator keyGen = new DataKeyGenerator();
-			keyGen.genKey("jifjdifdfd.jpg", cid, klen);
+			byte[] key = keyGen.genKey("jifjdifdfd.jpg", cid, klen);
+
+			handler = new CipherHandler(key, 16);
+			byte[] cdata = handler.cipher(StreamFileUtils.readBytes(in));
+			StreamFileUtils.writeBytes(cout, cdata);
+			
+			keyGen = new DataKeyGenerator();
+			key = keyGen.genKey("jifjdifdfd.jpg", cid, klen);
+			handler = new CipherHandler(key, 16);
+			byte[] dcdata = handler.decipher(StreamFileUtils.readBytes(cout));
+			StreamFileUtils.writeBytes(dout, dcdata);
 		}
 	}
 
 	void kcache(int num) {
 		for (int i = 0; i < num; i++) {
-			String fname = "jifjdifdfd" + num;
-			kcache.get(fname);
+			String fname = "jifjdifdfd-" + i;
+			byte[] key = kcache.get(fname);
+			handler = new CipherHandler(key, 16);
+			byte[] cdata = handler.cipher(StreamFileUtils.readBytes(in));
+			StreamFileUtils.writeBytes(cout, cdata);
+			
+			key = kcache.get(fname);
+			handler = new CipherHandler(key, 16);
+			byte[] dcdata = handler.decipher(StreamFileUtils.readBytes(cout));
+			StreamFileUtils.writeBytes(dout, dcdata);
 		}
 	}
 
 	void init(int num) {
 		for (int i = 0; i < num; i++) {
 			DataKeyGenerator keyGen = new DataKeyGenerator();
-			String fname = "jifjdifdfd" + num;
+			String fname = "jifjdifdfd-" + i;
 			byte[] key = keyGen.genKey(fname, cid, klen);
 			kcache.put(fname, key);
 		}
