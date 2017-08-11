422d50f52ae5e4c81bbe9cd651e67c7f4bbf37b8
lbcdev <lbcdev@yahoo.com>
2017-08-11 11:11:14 -0400

    simu raw i/o vs cipher i/o

--- src/org/sstore/server/test/TestCipherHandler.java
+++ src/org/sstore/server/test/TestCipherHandler.java
@@ -7,25 +7,44 @@ import org.sstore.utils.StreamFileUtils;
 
 public class TestCipherHandler {
 	private CipherHandler handler;
+	static String rootdir = "/Users/lbchen/sstoredata/";
 
 	public static void main(String[] args) {
 		TestCipherHandler handler = new TestCipherHandler();
 		int times = 5000;
+		handler.rawlgfile(times / 100);
 		handler.testsmfile(times);
 		handler.testmdfile((times / 10));
 		handler.testlgfile((times / 100));
 	}
 
+	public void rawlgfile(int times) {
+		// String in = rootdir + "got.png";
+		// String cout = rootdir + "cout.jpg";
+		// String dout = rootdir + "dout.jpg";
+		String in = rootdir + "gotlg.png";
+		String cout = rootdir + "coutlg.jpg";
+		String dout = rootdir + "doutlg.jpg";
+		long start = System.currentTimeMillis();
+		while (times-- > 0) {
+			byte[] cdata = StreamFileUtils.readBytes(in);
+			StreamFileUtils.writeBytes(cout, cdata);
+			byte[] dcdata = StreamFileUtils.readBytes(cout);
+			StreamFileUtils.writeBytes(dout, dcdata);
+		}
+		long end = System.currentTimeMillis();
+		System.out.println(end - start);
+	}
+
 	public void testsmfile(int times) {
 		long start = System.currentTimeMillis();
+		String in = rootdir + "got.png";
+		String cout = rootdir + "cout.jpg";
+		String dout = rootdir + "dout.jpg";
 		while (times-- > 0) {
 			String keystr = "a girl has no name";
 			byte[] key = keystr.getBytes();
 			handler = new CipherHandler(key, 16);
-			String rootdir = "/Users/lbchen/";
-			String in = rootdir + "got.png";
-			String cout = rootdir + "cout.jpg";
-			String dout = rootdir + "dout.jpg";
 			byte[] cdata = handler.cipher(StreamFileUtils.readBytes(in));
 			StreamFileUtils.writeBytes(cout, cdata);
 			byte[] dcdata = handler.decipher(StreamFileUtils.readBytes(cout));
@@ -36,15 +55,14 @@ public class TestCipherHandler {
 	}
 
 	public void testmdfile(int times) {
+		String in = rootdir + "gotmd.png";
+		String cout = rootdir + "coutmd.jpg";
+		String dout = rootdir + "doutmd.jpg";
 		long start = System.currentTimeMillis();
 		while (times-- > 0) {
 			String keystr = "a girl has no name";
 			byte[] key = keystr.getBytes();
 			handler = new CipherHandler(key, 16);
-			String rootdir = "/Users/lbchen/";
-			String in = rootdir + "gotmd.png";
-			String cout = rootdir + "coutmd.jpg";
-			String dout = rootdir + "doutmd.jpg";
 			byte[] cdata = handler.cipher(StreamFileUtils.readBytes(in));
 			StreamFileUtils.writeBytes(cout, cdata);
 			byte[] dcdata = handler.decipher(StreamFileUtils.readBytes(cout));
@@ -53,17 +71,16 @@ public class TestCipherHandler {
 		long end = System.currentTimeMillis();
 		System.out.println(end - start);
 	}
-	
+
 	public void testlgfile(int times) {
+		String in = rootdir + "gotlg.png";
+		String cout = rootdir + "coutlg.jpg";
+		String dout = rootdir + "doutlg.jpg";
 		long start = System.currentTimeMillis();
 		while (times-- > 0) {
 			String keystr = "a girl has no name";
 			byte[] key = keystr.getBytes();
 			handler = new CipherHandler(key, 16);
-			String rootdir = "/Users/lbchen/";
-			String in = rootdir + "gotlg.png";
-			String cout = rootdir + "coutlg.jpg";
-			String dout = rootdir + "doutlg.jpg";
 			byte[] cdata = handler.cipher(StreamFileUtils.readBytes(in));
 			StreamFileUtils.writeBytes(cout, cdata);
 			byte[] dcdata = handler.decipher(StreamFileUtils.readBytes(cout));
