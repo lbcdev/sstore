a7fcc5edd6a687ee115d6f476f985de22da441a2
lbcdev <lbcdev@yahoo.com>
2018-02-09 17:00:01 -0500

    packaging

---
+++ bin/run-benchmarks.sh
@@ -0,0 +1,1 @@
+mvn exec:java -D exec.mainClass=org.sstore.benchmarks.ConcurrentWorkload
---
+++ bin/start-dataserver.sh
@@ -0,0 +1,2 @@
+mvn exec:java -D exec.mainClass=org.sstore.server.storage.DataServer -D exec.args="localhost 1100"
+
